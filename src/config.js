4f378cf3b9b0e43bd5df8d018f19310c20407622
Kalev Keil <103848034+kodev8@users.noreply.github.com>
2024-05-16 20:22:00 +0200

    added release env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const result = dotenv.config({ path: '.dev.env'});
+const result = dotenv.config({ path: '.release.env'});
 
 if (result.error) {
   throw result.error;
