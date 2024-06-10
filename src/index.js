a133e8c89c27136f473f4c2aa81db59ecaaf4840
Kalev Keil <103848034+kodev8@users.noreply.github.com>
2024-06-10 19:24:11 +0200

    chore(services): enable cors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { DB_URI } = require('./config');
 const userRoutes = require('./routes/users.routes');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+app.options("*", cors());
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_URI, {
